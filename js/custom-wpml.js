0bfedce9e358744ae41441d1b3bf3226b71acbaa
pidev077 <nguyenanhphi077@gmail.com>
2024-12-09 10:40:38 +0700

    update

--- js/custom-wpml.js
+++ js/custom-wpml.js
@@ -5,5 +5,6 @@ jQuery(document).ready(function($) {
         $('li.tribe-events-c-view-selector__list-item--list span.tribe-events-c-view-selector__list-item-text').text('قائمة');
         $('.tribe-events-header__top-bar .tribe-events-c-top-bar__today-button').text('اليوم');
         $('.elementor-posts--skin-grid-toluca .elementor-post__content .elementor-post__meta li a span').text('بواسطة');
+        $('.elementor-filter-wrap.elementor-members--skin-pumori .elementor-filter__link[data-filter="all"]').text('فريقنا');
     }
 });
