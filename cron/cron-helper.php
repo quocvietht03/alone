ebf11ab4cc2071a43021a826b4b8a07fd46212a6
tomdevvn90 <tomdevvn90@gmail.com>
2025-03-03 14:54:35 +0700

    update cron event, give

--- cron/cron-helper.php
+++ cron/cron-helper.php
@@ -1,37 +1,67 @@
 <?php
-function alone_events_cron_exec() {
-  $wp_query = new \WP_Query( array(
-                          'post_type' => 'tribe_events',
-                          'post_status' => 'publish',
-                          'posts_per_page' => -1
-                        ) );
+function alone_events_cron_exec()
+{
+    $args_cron = [
+        'posts_per_page' => -1,
+    ];
 
-  if ( $wp_query->have_posts() ) {
-    while ( $wp_query->have_posts() ) {
-      $wp_query->the_post();
+    $events_cron = tribe_get_events($args_cron);
+    if (!empty($events_cron)) {
+        foreach ($events_cron as $event_cron) {
 
+            $event_id = $event_cron->ID;
 
+            $start_date = get_post_meta($event_id, '_EventStartDate', true);
+            $end_date = get_post_meta($event_id, '_EventEndDate', true);
 
-      $start_date = get_post_meta( get_the_ID(), '_EventStartDate', true);
-      $end_date = get_post_meta( get_the_ID(), '_EventEndDate', true);
-      
-      $today = date('Y-m-d H:i:s');
+            if (empty($start_date) || empty($end_date)) {
+                continue;
+            }
+            $new_start_date = date_create($start_date);
+            date_add($new_start_date, date_interval_create_from_date_string('7 days'));
+            $new_start_date = date_format($new_start_date, 'Y-m-d H:i:s');
 
-      if ($start_date < $today) {
-        $start_date = date_create($start_date);
-        date_add($start_date, date_interval_create_from_date_string('365 days'));
-        $start_date = date_format($start_date, 'Y-m-d H:i:s');
+            $new_end_date = date_create($end_date);
+            date_add($new_end_date, date_interval_create_from_date_string('7 days'));
+            $new_end_date = date_format($new_end_date, 'Y-m-d H:i:s');
 
-        $end_date = date_create($end_date);
-        date_add($end_date, date_interval_create_from_date_string('365 days'));
-        $end_date = date_format($end_date, 'Y-m-d H:i:s');
-
-        update_post_meta(get_the_ID(), '_EventStartDate', $start_date);
-        update_post_meta(get_the_ID(), '_EventEndDate', $end_date);
-
-      }
+            update_post_meta($event_id, '_EventStartDate', $new_start_date);
+            update_post_meta($event_id, '_EventEndDate', $new_end_date);
+            update_post_meta($event_id, '_EventStartDateUTC', $new_start_date);
+            update_post_meta($event_id, '_EventEndDateUTC', $new_end_date);
+        }
     }
-  }
-
 }
 add_action('alone_events_cron_hook', 'alone_events_cron_exec');
+
+
+function alone_donations_cron_exec()
+{
+    $args = array(
+        'number' => 100,
+    );
+    $donor_query = new Give_Donors_Query($args);
+    $donor_query = $donor_query->get_donors();
+    $payment_arr = array();
+    $specific_date = '2025-01-01 00:00:00';
+    if ($donor_query) {
+        foreach ($donor_query as $donor) {
+            if ($donor->date_created > $specific_date) {
+                $payment_arr = explode(',', $donor->payment_ids);
+                foreach ($payment_arr as $payment_id) {
+                    give_delete_donation($payment_id);
+                }
+                Give()->donors->delete($donor->id);
+            } else {
+                $payment_arr = explode(',', $donor->payment_ids);
+                foreach ($payment_arr as $payment_id) {
+                    $payment = get_post($payment_id);
+                    if ($payment && $payment->post_date > $specific_date) {
+                        give_delete_donation($payment_id);
+                    }
+                }
+            }
+        }
+    }
+}
+add_action('alone_donations_cron_hook', 'alone_donations_cron_exec');
